936c2f558ace96a237bf4ffba825fd0f5e93baa3
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2023-06-08 11:12:20 +0500

    header fixed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+
+$(window).scroll(function(){
+    if ( $(this).scrollTop() > 50) {
+        $('.header').addClass("fixed");
+    } else {
+        $('.header').removeClass("fixed");
+    }
+});
 $(".banner__burgir").click(function() {
     $(".header__navbar").addClass("active");
     setTimeout(function(){
